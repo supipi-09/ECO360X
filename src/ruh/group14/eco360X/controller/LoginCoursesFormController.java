8569c4fc7136f05e27f99ed7f943d5bbfa95747e
Janith Ranasinghe <janithraditha@gmail.com>
2024-02-19 15:37:31 +0530

    Add new ui featurs
    make a course form and related forms

--- src/ruh/group14/eco360X/controller/LoginCoursesFormController.java
+++ src/ruh/group14/eco360X/controller/LoginCoursesFormController.java
@@ -1,4 +1,36 @@
 package ruh.group14.eco360X.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginCoursesFormController {
+    public AnchorPane context;
+
+    public void startNow_1_OnAction(ActionEvent actionEvent) throws IOException {
+        setUi("Corse_1Form");
+    }
+
+    public void startNow_2_OnAction(ActionEvent actionEvent) throws IOException {
+        setUi("Corse_2Form");
+    }
+
+    public void startNow_3_OnAction(ActionEvent actionEvent) throws IOException {
+        setUi("Corse_3Form");
+    }
+
+    public void startNow_4_OnAction(ActionEvent actionEvent) throws IOException {
+        setUi("Corse_4Form");
+    }
+
+    private void setUi(String location) throws IOException {
+        context.getChildren().clear();
+        context.getChildren().add(
+                FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))
+        );
+    }
 }
