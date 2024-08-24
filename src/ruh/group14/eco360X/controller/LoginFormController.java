18526b4077023569a1b2b53527fbadb7f0a25884
Janith Ranasinghe <janithraditha@gmail.com>
2024-08-24 22:32:43 +0530

    create UserSelect and complete ProfileFormController

--- src/ruh/group14/eco360X/controller/LoginFormController.java
+++ src/ruh/group14/eco360X/controller/LoginFormController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import ruh.group14.eco360X.db.Database;
+import ruh.group14.eco360X.db.UserSelect;
 import ruh.group14.eco360X.model.User;
 import ruh.group14.eco360X.util.Security.PasswordManager;
 
@@ -35,11 +36,11 @@ public class LoginFormController {
         String password = txtPassword.getText().trim();
 
         try {
-            User selectedUser = login(email);
+            User selectedUser = new UserSelect().login(email);
             if (null != selectedUser) {
                 if (new PasswordManager().checkPassword(password, selectedUser.getPassword())) {
                     setUi("LoginDashBordForm");
-                    Database.userTable.add(selectedUser);
+                    ProfileFormController.userEmail = selectedUser.getEmail();
                     new Alert(Alert.AlertType.INFORMATION, "Login Successful").show();
                 } else {
                     new Alert(Alert.AlertType.ERROR, "Wrong Password").show();
@@ -50,28 +51,6 @@ public class LoginFormController {
         } catch (SQLException | ClassNotFoundException e) {
             new Alert(Alert.AlertType.WARNING, e.toString()).show();
         }
-    }
-
-    private User login(String email) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection =
-                DriverManager.getConnection("jdbc:mysql://localhost:3306/wildCourse_lms", "root", "1234");
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE email = ?");
-        preparedStatement.setString(1, email);
-        ResultSet resultSet = preparedStatement.executeQuery();
-
-        if (resultSet.next()) {
-            User user = new User(
-                    resultSet.getString("fName"),
-                    resultSet.getString("lName"),
-                    resultSet.getString("address"),
-                    resultSet.getString("nic"),
-                    resultSet.getString("email"),
-                    resultSet.getString(6)
-            );
-            return user;
-        }
-        return null;
     }
 
     private void setUi(String location) throws IOException {
--- src/ruh/group14/eco360X/controller/ProfileFormController.java
+++ src/ruh/group14/eco360X/controller/ProfileFormController.java
@@ -7,42 +7,37 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import ruh.group14.eco360X.db.Database;
+import ruh.group14.eco360X.db.UserSelect;
 import ruh.group14.eco360X.model.User;
 
 import java.io.IOException;
 import java.sql.*;
 
 public class ProfileFormController {
-
-
     public AnchorPane context;
     public Label lblEmail;
     public Label lblFirstName;
     public Label lblLastName;
     public Label lblAddress;
     public Label lblNic;
+    public static String userEmail;
 
-    public void initialize(){
-        setData();
-    }
-    public static String loginEmail="";
-
-    private void setData(){
-        for(User tmp : Database.userTable){
-            if(tmp.getEmail().equals("oshada@gmail.com")){
-                lblFirstName.setText(tmp.getFirstName());
-                lblLastName.setText(tmp.getLasttName());
-                lblAddress.setText(tmp.getAddress());
-                lblNic.setText(tmp.getNic());
-                lblEmail.setText(tmp.getEmail());
-            }
+    public void initialize() {
+        try {
+            User user =new UserSelect().login(userEmail);
+            lblAddress.setText(user.getAddress());
+            lblEmail.setText(user.getEmail());
+            lblNic.setText(user.getNic());
+            lblFirstName.setText(user.getFirstName());
+            lblLastName.setText(user.getLasttName());
+        }catch (SQLException | ClassNotFoundException e){
+            System.out.println(e);
         }
-
     }
 
     private void SetUi(String location) throws IOException {
         Stage stage = (Stage) context.getScene().getWindow();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml"))));
         stage.centerOnScreen();
     }
 
